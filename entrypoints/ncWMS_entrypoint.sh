50c291d26a66aef90070110d3104896a3d22d143
MarcoAlbaETT <marco.alba@ettsolutions.com>
2025-07-03 17:01:21 +0200

    tomcat password fix
    tomcat password fix

--- entrypoints/ncWMS_entrypoint.sh
+++ entrypoints/ncWMS_entrypoint.sh
@@ -42,8 +42,9 @@ if [ "$1" = 'start-tomcat.sh' ] || [ "$1" = 'catalina.sh' ]; then
     pwd=$(echo "$pwd"|grep $ncwmspassword)
     IFS=':'
     read -ra ADDR <<<"$pwd"
-    pwd=${ADDR[1]}
-    sed -i "s/823f3f8e9a2727375993229e4a71593618441c07/$pwd/g" /usr/local/tomcat/conf/tomcat-users.xml
+    pwd=${ADDR[1]}    
+    sed -i "s/xxx/$pwd/g" /usr/local/tomcat/conf/tomcat-users.xml
+    #sed -i "s/823f3f8e9a2727375993229e4a71593618441c07/$pwd/g" /usr/local/tomcat/conf/tomcat-users.xml
 
     exec gosu tomcat "$@"
 fi
